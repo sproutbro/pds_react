41f60ebc2e5d65aa752532f61ae4e1dc32f3c9fa
guts <krsproutbro@gmail.com>
2022-12-08 22:10:26 +0900

    env 설정변경

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import { Container } from "react-bootstrap";
 import AppNav from "./components/AppNav";
 import AppRoutes from "./routes/AppRoutes";
 import axios from "axios";
-axios.defaults.baseURL='http://localhost:8080';
+axios.defaults.baseURL=process.env.REACT_APP_BASE_URL;
 
 function App() {
-  
   return (
     <>
       <AppNav />
