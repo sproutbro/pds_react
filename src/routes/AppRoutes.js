800178d30f5e796fd46deba46fb94cca27322f6f
guts <krsproutbro@gmail.com>
2022-12-09 03:08:09 +0900

    env 설정변경

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -6,7 +6,7 @@ import Join from "../pages/Join";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>/</div>}></Route>
+      <Route path="/" element={<div>ㄴㅇ러ㅑㅐㅇㄴ러ㅑㅐㄴㅇ러ㅑㅐ</div>}></Route>
       <Route path="/plan" element={<Plan />}></Route>
       <Route path="/do" element={<div>do</div>}></Route>
       <Route path="/see" element={<div>see</div>}></Route>
