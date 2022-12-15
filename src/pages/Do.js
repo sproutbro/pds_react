dad75d2255e63fa513e109688444722c845c8a26
toting <yoonzz123@gmail.com>
2022-12-15 18:19:33 +0900

    add Button  - toting

--- src/pages/Do.js
+++ src/pages/Do.js
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { Card } from "react-bootstrap";
 
 const Do = () => {
+
+
+  const doAction = () => {
+    axios.get("/test")
+    .then(res => {
+      console.log("121212");
+      alert(res.data);
+    })
+    .catch(err => alert(err))
+  }
+  
+
   return (
+    <>
+    <Card>
     <div>dotest</div>
+    <button variant="primary" onClick={doAction}>
+            버튼
+    </button>
+    </Card>
+    </>
   )
 }
 
